725eaa0495307e6f1e266b63ed0e067b2c210259
jiang1991 <jiang12196@126.com>
2018-02-11 15:12:49 +0800

    react-native FlatList{

--- App.js
+++ App.js
@@ -6,6 +6,9 @@
 
 import React, { Component } from 'react';
 import {
+  AppRegistry,
+  Image,
+  FlatList,
   Platform,
   StyleSheet,
   Text,
@@ -19,40 +22,99 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json';
+
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    this.getMockData();
+  }
+
+  getMockData = () => {
+    fetch(REQUEST_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('data---->', data);
+      this.setState({
+        data: this.state.data.concat(data.movies),
+        loaded: true,
+      })
+    })
+  }
   render() {
+    if(!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          my first react-native
-        </Text>
-        <Text style={styles.instructions}>
-          thanks 
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderMovie}
+        style={styles.list}
+        keyExtractor={(movie) => movie.id}
+      />
     );
   }
+  
+  renderLoadingView = () => {
+    return (
+      <View style={styles.container}>
+      <Text>
+        Loading...
+      </Text>
+    </View>
+    );
+  };
+
+  renderMovie = (movie) => {
+    return (
+      <View style={styles.container} key={movie.item.id}>
+        <Image 
+          source={{uri: movie.item.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.item.title}</Text>
+          <Text style={styles.year}>{movie.item.year}</Text>
+        </View>
+    </View>
+    );
+  };
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
   },
-  instructions: {
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  list: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
